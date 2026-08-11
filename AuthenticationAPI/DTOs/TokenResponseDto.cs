32978a26b34b5c43db4ce5cd384226a43922639d
Pedro Miquelin <pm.miquelin@gmail.com>
2026-08-11 16:26:36 -0300

    Create DTOs

---
+++ AuthenticationAPI/DTOs/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthenticationAPI.DTOs
+{
+    public class TokenResponseDto
+    {
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
